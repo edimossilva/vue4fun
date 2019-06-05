151ffae3a31d769b43293474d8644529bea00fbb
edimossilva <edimossilva@gmail.com>
2019-06-04 23:52:56 -0300

    Creating project

---
+++ src/components/grower/grower-list.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1> Growers List </h1>
+    <h2 v-for="grower in growers" v-bind:key="grower.id">
+      {{grower.name}} - CPF:{{grower.cpf}}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GrowerList',
+  data () {
+    return {
+      growers: [],
+
+
+    }
+  },
+  methods: {
+    getGrowers() {
+      this.$http.get('https://my-json-server.typicode.com/pedroskakum/fake-api/grower').
+      then(response => growers = this.getGrowersFromJson(response.body));
+    },
+    getGrowersFromJson(json) {
+      this.growers = json;
+    }
+
+  },
+  mounted: function () {
+    growers = this.getGrowers();
+  }
+}
+</script>
+
+<style>
+
+</style>
